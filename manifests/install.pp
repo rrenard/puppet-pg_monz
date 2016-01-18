2130dc5365aa3fa1b9552bd0363a65fdab5a9667
Shaun Rampersad <shaun.rampersad@fnb.co.za>
2016-01-18 11:29:35 +0200

    Removed notify in favour of subscribe

--- manifests/install.pp
+++ manifests/install.pp
@@ -22,34 +22,34 @@ class pg_monz::install {
     group => $::pg_monz::zabbix_group,
   } ->
 
-  file { "/usr/local/bin/pg_monz" :
+  file { '/usr/local/bin/pg_monz' :
     ensure  => link,
     owner   => $::pg_monz::zabbix_user,
     group   => $::pg_monz::zabbix_group,
     target  => "${::pg_monz::install_dir}/pg_monz-${::pg_monz::version}/pg_monz/usr-local-bin",
   } ->
 
-  file { "/etc/pg_monz" :
+  file { '/etc/pg_monz' :
     ensure  => directory,
     owner   => $::pg_monz::zabbix_user,
     group   => $::pg_monz::zabbix_group,
     mode    => '0750',
   } ->
 
-  file { "/etc/pg_monz/pgsql_funcs.conf" :
+  file { '/etc/pg_monz/pgsql_funcs.conf' :
     ensure  => present,
     owner   => $::pg_monz::zabbix_user,
     group   => $::pg_monz::zabbix_group,
-    mode    => "0600",
-    content => template("pg_monz/pgsql_funcs.conf.erb"),
+    mode    => '0600',
+    content => template('pg_monz/pgsql_funcs.conf.erb'),
   } ->
 
-  file { "/etc/pg_monz/pgpool_funcs.conf" :
+  file { '/etc/pg_monz/pgpool_funcs.conf' :
     ensure  => present,
     owner   => $::pg_monz::zabbix_user,
     group   => $::pg_monz::zabbix_group,
-    mode    => "0600",
-    content => template("pg_monz/pgpool_funcs.conf.erb"),
+    mode    => '0600',
+    content => template('pg_monz/pgpool_funcs.conf.erb'),
   }
 
   $data = $::pg_monz::userparameters
@@ -61,11 +61,12 @@ class pg_monz::install {
     mode    => '0640',
     content => template('pg_monz/userparameter_pg_monz.conf.erb'),
     require => Staging::Extract["pg_monz-${::pg_monz::version}.tar.gz"],
-  } ~>
+  }
 
-  exec { "zabbix-agent-reload":
-    command => "/etc/init.d/zabbix-agent restart",
-    user    => "root",
+  exec { 'zabbix-agent-reload':
+    command => '/etc/init.d/zabbix-agent restart',
+    user    => 'root',
+    subscribe => File['/etc/zabbix/zabbix_agentd.d/userparameter_pg_monz.conf'],
   }
 
   
